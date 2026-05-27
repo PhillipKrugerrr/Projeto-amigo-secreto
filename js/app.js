ee3c46697e7b07283bca7b66ac9eb0e69e5467b4
Phillip Kruger <galeradapesada-999@hotmail.com>
2026-05-27 19:51:41 +0200

    First commit

---
+++ js/app.js
@@ -0,0 +1,118 @@
+
+/*let minhaLista = [1, 2, 3, 3, 4];
+let minhaListaDois = [4, 5, 6];
+let novaLista = minhaLista.concat(minhaListaDois);
+
+    function fisherYates(novaLista) {
+        for (let i = novaLista.length - 1; i > 0; i--) {
+            const j = Math.trunc(Math.random() * (i + 1));
+            [novaLista[i], novaLista[j]] = [novaLista[j], novaLista[i]];
+        }
+
+     novaLista;
+
+    console.log(novaLista);
+    
+    return;
+}
+
+//fisherYates(novaLista);
+
+function removerDuplicatas(minhaLista) {
+    return [...new Set(minhaLista)];
+}
+   let novaListaDuplicada = removerDuplicatas(minhaLista);
+   console.log(novaListaDuplicada);
+*/
+
+
+
+let nomes = [];
+const temNumero = /\d/;
+function adicionar() {
+    let novoNome = document.getElementById("nome-amigo").value;
+    let amigosIncluidos = document.getElementById("lista-amigos");
+
+        if (novoNome.trim() === "") {
+            alert('Nenhum nome adicionado');
+            return;
+        } 
+        
+        if (temNumero.test(novoNome)) {
+            alert('Nao é permitido o uso de números');
+            return;
+        }  
+        
+        if (nomes.includes(novoNome)) {
+            alert("Nome já adicionado!");
+            return; 
+        } 
+            nomes.push(novoNome);
+            amigosIncluidos.innerHTML = nomes.join(" , ");
+            limpar();
+    }
+
+function sortear() {
+
+    if (nomes.length <= 4) {
+        alert("Adicione pelo menos 4 amigos!");
+        return;
+    }
+
+    let listaSorteio = document.getElementById("lista-sorteio");
+    let nomesEmbaralhados = []; 
+
+        for (const pessoa of nomes) {
+            nomesEmbaralhados.push(pessoa);
+        }
+        for (let i = 0; i < nomesEmbaralhados.length; i++) {
+
+            let indiceAleatorio = Math.trunc(Math.random() * nomesEmbaralhados.length -1);
+
+            let temp = nomesEmbaralhados[i];
+            nomesEmbaralhados[i] = nomesEmbaralhados[indiceAleatorio];
+            nomesEmbaralhados[indiceAleatorio] = temp;
+    }
+            let resultado = "";
+
+        for (let i = 0; i < nomesEmbaralhados.length; i++) {
+            
+            let amigoSecreto;
+
+        if (i === nomesEmbaralhados.length - 1) {
+
+            amigoSecreto = nomesEmbaralhados[0];
+
+        } else {
+
+            amigoSecreto = nomesEmbaralhados[i + 1];
+
+        }
+
+            resultado += `${nomesEmbaralhados[i]} → ${amigoSecreto}<br>`;
+    }
+
+    listaSorteio.innerHTML = resultado;
+
+}
+function reiniciar() {
+    nomes = [];
+    document.getElementById("lista-amigos").innerHTML = "";
+    document.getElementById("lista-sorteio").innerHTML = "";
+    limpar()
+}
+
+function limpar() {
+    document.getElementById("nome-amigo").value = "";
+}
+
+let remover = document.getElementById('lista-amigos');
+
+remover.addEventListener('click', function() {
+    
+    remover.innerHTML = '';
+
+    limpar();
+});
+
+
